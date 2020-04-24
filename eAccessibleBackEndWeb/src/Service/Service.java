21a5cce2ef216e5834686380bfd215942be67cf9
MarCastellnou <marccastellnou98@gmail.com>
2020-04-24 22:14:39 +0200

    getCharacteristics DONE

--- eAccessibleBackEndWeb/src/Service/Service.java
+++ eAccessibleBackEndWeb/src/Service/Service.java
@@ -1,6 +1,7 @@
 package Service;
 
 import models.Accessibilitat;
+import models.CaracteristicaTipoLocal;
 import models.Local;
 import utils.UtilService;
 
@@ -443,5 +444,56 @@ public class Service {
 		return locals;
 	}
 	
+	
+	@WebMethod
+	public List<CaracteristicaTipoLocal> getCharacteristics(Integer codiTipoLocal) throws BasicException{
+		List<CaracteristicaTipoLocal> characteristics = new ArrayList<CaracteristicaTipoLocal>();
+		Connection connection = null;
+		
+		try {
+			InitialContext context = new InitialContext();
+			if(context != null) {
+				DataSource datasource = (DataSource) context.lookup( "java:jboss/PostgreSQL/eAccessible");
+				if(datasource == null) {
+					throw new BasicException(500,"No s'ha pogut establir un DataSource/Lookup.");
+				}else {
+					try {
+						connection = datasource.getConnection();
+					}catch(Exception ex) {
+						throw new BasicException(444,"No s'ha pogut establir connexio amb la base de dades.");
+					}
+					
+					String query = "select * from eAccessible.caracteristicatipolocal where caracteristicatipolocal.coditipolocal="+codiTipoLocal;
+					
+					try {
+						Statement state = connection.createStatement();
+						ResultSet rs = state.executeQuery(query);
+						while(rs.next()) {
+							CaracteristicaTipoLocal caracteristicaTipoLocal = new CaracteristicaTipoLocal();
+							caracteristicaTipoLocal.setCodiCaracteristicaTipoLocal(rs.getInt("codicaracteristicatipolocal"));;
+							caracteristicaTipoLocal.setCodiCaracteristica(rs.getInt("codicaracteristica"));
+							caracteristicaTipoLocal.setCodiTipoLocal(rs.getInt("coditipolocal"));
+							characteristics.add(caracteristicaTipoLocal);
+						}
+						state.close();
+					}catch(Exception ex) {
+						throw new BasicException(500,"No s'ha pogut crear un Statement o error en la query. SQL exception");
+					}
+					connection.close();
+				}
+			}
+			
+		} catch(Exception exception) {
+			throw new BasicException(500, "No s'ha pogut trobar locals amb el nom de local introduit.");
+		} finally {
+			try {
+				connection.close();
+			} catch (SQLException ex) {
+				throw new BasicException(500,ex.toString());
+			}
+		}
+		
+		return characteristics;
+	}
 	
 }
